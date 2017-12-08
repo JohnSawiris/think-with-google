6c31d5b7b7e2fa0d7d10b2e936e5ca480cd6e1d5
John Sawiris <johnedwardsawiris@gmail.com>
2017-12-07 19:39:17 -0800

    fix the navbar bug when resizing browser window and refactor the javascript code

--- js/app.js
+++ js/app.js
@@ -2,18 +2,35 @@ document.addEventListener('DOMContentLoaded', function() {
   var navBtn = document.querySelector('.nav-bar-btn');
   var closeBtn = document.querySelector('.close-btn');
   var navLinks = document.querySelector('.nav-links');
+  var body = document.getElementsByTagName('body')[0];
+
+  function show() {
+    navLinks.style.width = '400px';
+    navLinks.style.opacity = 1;
+    navLinks.style.display = 'block';
+    closeBtn.style.display = 'block';
+  }
+
+  function hide() {
+    navLinks.style.width = 0;
+    navLinks.style.opacity = 0;
+    closeBtn.style.display = 'none';
+  }
+
   //Events
   navBtn.addEventListener('click', function() {
-      navLinks.style.width = '42.6%';
-      navLinks.style.display = 'block';
-      closeBtn.style.display = 'block';
-      this.classList.toggle('click');
-
+      show();
   });//NavBtn click end
 
   closeBtn.addEventListener('click', function() {
-    navLinks.style.width = '0';
-    this.style.display = 'none';
-    navBtn.classList.toggle('click');
+    hide();
   });//CloseBtn click end
+  window.addEventListener('resize', function() {
+    if(window.innerWidth > 990) {
+      navLinks.style.opacity = 1;
+      navLinks.style.width = "auto";
+    } else if (window.innerWidth < 990) {
+      hide();
+    }
+  });
 });//DoMContentLoaded end
